2e2472a748693675dcc7a2f71806bafaf9599758
Benjamin Salon <bsalon@redhat.com>
2020-07-15 11:27:45 +0200

    settings.sh: settings of the test
    Settings file provides some kind of customization.

---
+++ settings.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# TODO: depth, more commands, colors
+
+#### which perf to use in the testing
+export CMD_PERF=${CMD_PERF:-`which perf`}
+
+#### program to be examinated by perf
+export COMMAND="true"
+
+
